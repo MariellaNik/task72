cb53156ad4cb15afc6fe03eb10e9d375ccfe61b4
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-02-15 12:59:51 +0200

    Create Tags.js

---
+++ src/Tags.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// Tags component
+export function Tags({ tags }) {
+    //the Tags component receives a tags prop, which is an array of strings
+    const defaultTags = ['boomdotdev', 'task', 'tags', 'react'];
+    const allTags = tags || defaultTags;
+    //If the tags prop is not provided, the component will use the default tags specified in the defaultTags array.
+  
+    return (
+      <div className="tags">
+        {allTags.map((tag) => (
+          <span key={tag} className="tag">
+            #{tag}
+          </span>
+        ))}
+      </div>
+    );
+  }
+  
+  //export default Tags;
