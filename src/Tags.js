28cab1652cd3920cb9d7c6e57b6da1e54fa46a67
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-02-22 13:21:45 +0200

    Update Tags.js

--- src/Tags.js
+++ src/Tags.js
@@ -1,15 +1,10 @@
-import React from "react";
-
-const Tags = (props) => {
-  const { tags } = props;
-  return (
-    <div className="tags">
-      {tags.map((tag) => (
-        <span className="tag">#{tag}</span>
-      ))}
-    </div>
-  );
-};
-
-export default Tags;
+export const Tags = (props) => {
+    return (
+        <ul className="tags">
+            {props.tags.map((tag, index) => {
+                return <li key={ index } className="tag">{"# " +  tag }</li>
+            })}
+        </ul>
+    )
+}
 
